f254ee44131bcb7ed4b66f808584686ecbc1999f
Miranda <flowerbutterfly8@gmail.com>
2025-07-28 18:25:52 -0400

    got the image type conversion to stop erroring, next to get it to show up

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import os
 
 from pdfManagement.imageFromPDF import convert_to_image
-# from pdfManagement.convertImageToMultiPagePdf import getPageBoundaries
+from pdfManagement.convertImageToMultiPagePdf import export_multi_page_pdf
 from visionComponents.getIndividualPieces import find_pieces_from_image_file
+from reportlab.lib.pagesizes import letter
 
 def runProcessing(dirPath):
   files = filter(lambda x: x.endswith(".pdf"), os.listdir(dirPath))
@@ -16,13 +17,13 @@ def runProcessing(dirPath):
   
   print(imageFiles)
   
-  for imageFile in imageFiles:
-    pieces = find_pieces_from_image_file(imageFile)
+  pieces = []
 
-  for image in images:
-    # TODO split pieces into multiple pages that are printable
-    # bounds = getPageBoundaries(image)
-    pass
+  for imageFile in imageFiles:
+    pieces.extend(find_pieces_from_image_file(imageFile))
+
+  for image in pieces:
+    export_multi_page_pdf(image, letter)
 
 
 runProcessing("testFiles")
--- pdfManagement/convertImageToMultiPagePdf.py
+++ pdfManagement/convertImageToMultiPagePdf.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python 
 from pdf2image import convert_from_path
-from ..visionComponents.outputOntoImage import output_boxes_on_image
+import cv2 as cv
+import numpy as np
+from reportlab.pdfgen import canvas
+from PIL import Image
+from io import BytesIO 
+from reportlab.lib.utils import ImageReader
 
-def getPageBoundaries(image):
-  bounds = []
-  return bounds
+def divide_image(image, page_size):
+  #TODO divide image up into a list of images
+  return [image]
+
+def convert_image(numpy_img):
+  return Image.fromarray(numpy_img)
+
+def export_multi_page_pdf(image, page_size):
+  page_x, page_y = page_size
+  print(page_size)
+  print(image.shape)
+
+  doc = canvas.Canvas("testFiles/test.pdf", pagesize=page_size)
+  split_images = divide_image(image, page_size)
+  for img in split_images:
+    doc.drawInlineImage(convert_image(img), page_x, page_y, page_x, page_y)
+    doc.showPage()
+  doc.save()
+
+if __name__ == "__main__":
+  image_file = 'testFiles/BodiceDartedSleeved_GH_A0_1105Upton.png'
+  image = cv.imread(image_file)
+
+  from reportlab.lib.pagesizes import letter # Size at 72 DPI
+  export_multi_page_pdf(image, letter)
+
