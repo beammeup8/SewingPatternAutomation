1e6a5893f522750945c24cea1b579cc161b5a5af
Miranda <flowerbutterfly8@gmail.com>
2025-08-03 17:34:08 -0400

    added to the readme, and some progress

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Sewing Pattern Automation
 Code to generate sewing patterns, as well as making it easier to work with existing ones
+
+## Dependencies
+
+### Open CV
+```bash
+pip install opencv-python
+```
+
+### Pdf 2 Image
+```bash
+pip install pdf2image
+```
+
+### Report Lab
+```bash
+pip install reportlab
+```
+
--- pdfManagement/convertImageToMultiPagePdf.py
+++ pdfManagement/convertImageToMultiPagePdf.py
@@ -9,25 +9,31 @@ from reportlab.lib.utils import ImageReader
 import time
 import os
 
-def divide_image(image, page_size):
-  #TODO divide image up into a list of images
-  return [image]
+REPORT_LAB_DPI = 72
+image_count = 0
 
-def convert_image(numpy_img):
-  new_file_name = f"testFiles/image_{time.time()}.png"
+def divide_image(image, page_size, image_size_inch):
+  pages = []
+  image_dpi = image.shape[0]/image_size_inch[0]
+
+
+  return pages
+def convert_images(numpy_img):
+  new_file_name = f"testFiles/tmp_image_{image_count}.png"
+  image_count +=1
   cv.imwrite(new_file_name, numpy_img)
   return new_file_name
 
-def export_multi_page_pdf(image, page_size):
+def export_multi_page_pdf(image, page_size, image_size_inches):
   page_x, page_y = page_size
   print(page_size)
   print(image.shape)
 
   doc = canvas.Canvas("testFiles/test.pdf", pagesize=page_size)
-  split_images = divide_image(image, page_size)
+  split_images = divide_image(image, page_size, image_size_inches)
   for img in split_images:
     file_name = convert_image(img)
-    drawn_size = doc.drawImage(file_name, 0, 0, 1000, 1000)
+    drawn_size = doc.drawImage(file_name, 0, 0)
     # The library will not accept a file in tmp, so this is the work around
     os.remove(file_name)
     doc.showPage()
@@ -37,7 +43,8 @@ def export_multi_page_pdf(image, page_size):
 if __name__ == "__main__":
   image_file = 'testFiles/BodiceDartedSleeved_GH_A0_1105Upton.png'
   image = cv.imread(image_file)
+  a0_inches = (33.1, 46.8)
 
   from reportlab.lib.pagesizes import letter # Size at 72 DPI
-  export_multi_page_pdf(image, letter)
+  export_multi_page_pdf(image, letter, a0_inches)
 
