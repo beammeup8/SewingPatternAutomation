42e048798f32523e714695efc732852d6a673cc6
Miranda <flowerbutterfly8@gmail.com>
2025-08-03 18:29:21 -0400

    now getting the right page count, but not content

--- pdfManagement/convertImageToMultiPagePdf.py
+++ pdfManagement/convertImageToMultiPagePdf.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python 
 from pdf2image import convert_from_path
 import cv2 as cv
-import numpy as np
 from reportlab.pdfgen import canvas
-from PIL import Image
-from io import BytesIO 
 from reportlab.lib.utils import ImageReader
-import time
 import os
+import math
 
 REPORT_LAB_DPI = 72
-image_count = 0
 
 def divide_image(image, page_size, image_size_inch):
   pages = []
   image_dpi = image.shape[0]/image_size_inch[0]
+  conversion_factor = image_dpi / REPORT_LAB_DPI
+  page_x = round(page_size[0] * conversion_factor)
+  page_y = round(page_size[1] * conversion_factor)
 
+  print((page_x, page_y))
+
+  x_page_count = math.ceil(image.shape[0]/page_x)
+  y_page_count = math.ceil(image.shape[1]/page_y)
+
+  for x in range(x_page_count):
+    x_start = x * page_x
+    x_end = x_start + page_x
+    for y in range(y_page_count):
+      y_start = y * page_y
+      pages.append(image[x_start:x_end, y_start:y_start + page_y])
 
   return pages
-def convert_images(numpy_img):
-  new_file_name = f"testFiles/tmp_image_{image_count}.png"
-  image_count +=1
+
+def convert_image(numpy_img):
+  new_file_name = f"testFiles/tmp_image.png"
   cv.imwrite(new_file_name, numpy_img)
   return new_file_name
 
 def export_multi_page_pdf(image, page_size, image_size_inches):
-  page_x, page_y = page_size
   print(page_size)
   print(image.shape)
 
@@ -37,7 +46,6 @@ def export_multi_page_pdf(image, page_size, image_size_inches):
     # The library will not accept a file in tmp, so this is the work around
     os.remove(file_name)
     doc.showPage()
-    print(drawn_size)
   doc.save()
 
 if __name__ == "__main__":
