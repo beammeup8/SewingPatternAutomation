a2e74ce8d0144206c81dc246b5e2e4788a9e6428
Miranda <flowerbutterfly8@gmail.com>
2025-08-15 21:11:58 -0400

    generating page numbers correctly need to figure out how to superimpose them such that they are greyed out

--- pdfManagement/convertImageToMultiPagePdf.py
+++ pdfManagement/convertImageToMultiPagePdf.py
@@ -6,6 +6,7 @@ from reportlab.lib.utils import ImageReader
 import os
 import math
 import time
+from string import ascii_uppercase as letters
 
 REPORT_LAB_DPI = 72
 BORDER_INCHES = 0.5
@@ -40,7 +41,8 @@ def divide_image(image, page_size, image_size_inch):
             x_end = min(x_start + page_width_px, img_width_px)
             pages.append(image[y_start:y_end, x_start:x_end])
 
-    return pages
+    return pages, x_page_count, y_page_count
+
 
 def convert_image(numpy_img):
   new_file_name = f"testFiles/tmp_image{time.time()}.png"
@@ -54,8 +56,25 @@ def export_multi_page_pdf(image, page_size, image_size_inches):
   usable_height = page_size[1] - 2 * BORDER_POINTS
 
   doc = canvas.Canvas("testFiles/test.pdf", pagesize=page_size)
-  split_images = divide_image(image, (usable_width, usable_height), image_size_inches)
+  split_images, pages_x, pages_y = divide_image(image, (usable_width, usable_height), image_size_inches)
+  
+  current_page_x = 0
+  current_page_y = 0
+  current_letter = letters[current_page_y]
+
   for img in split_images:
+    if current_page_x > pages_x - 1:
+       current_page_x = 1
+       current_page_y += 1
+       if current_page_y > len(letters):
+          wrap_count = current_page_y / len(letters)
+          current_letter = letters[wrap_count - 1] + letters[current_page_y % len(letters)]
+       else:
+          current_letter = letters[current_page_y]
+
+    else:
+       current_page_x += 1
+
     file_name = convert_image(img)
     doc.setStrokeColorRGB(0, 0, 0)
     doc.setLineWidth(2)
